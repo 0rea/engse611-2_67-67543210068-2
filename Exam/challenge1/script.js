caf591b4b5968def36f44a2b5dff3e6a1ded7383
0rea <notae258@gmail.com>
2025-03-18 14:32:02 +0700

    D

--- Exam/challenge1/script.js
+++
@@ -1,7 +0,0 @@
-const paragraphs = document.querySelectorAll("#main p");
-
-  // Loop through each selected paragraph and apply styles
-  paragraphs.forEach(p => {
-    p.style.fontSize = "24px";
-    p.style.color = "red";
-  });
